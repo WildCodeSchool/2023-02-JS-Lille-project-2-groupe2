5d206cc655b497095d0920d2560bfab3422e2804
Alexandre Delbarre <alexandre.delbarre@hotmail.fr>
2023-04-28 10:42:49 +0200

    correct

--- frontend/src/components/ShowPictures/ShowPictures.jsx
+++ frontend/src/components/ShowPictures/ShowPictures.jsx
@@ -1,128 +1,166 @@
-import React, { useState, useEffect, useRef } from "react";
-import { v4 as uuidv4 } from "uuid"; // import uuid library
 import "./ShowPictures.scss";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid"; // import uuid library
+import { shuffle } from "lodash";
 
 const imageList = [
   {
+    id: "imgbaba",
     name: "baba",
-    url: "https://www.wallpapers13.com/wp-content/uploads/2015/12/Nature-Lake-Bled.-Desktop-background-image-840x525.jpg",
+    url: "",
+    stat: "",
+  },
+  { id: "kgjfvifh", name: "dudu", url: "", stat: "" },
+  {
+    id: "pepepepep",
+    name: "baba",
+    url: "",
     stat: "",
   },
   {
+    id: "jvifojiojvjf",
     name: "dudu",
-    url: "https://www.wallpapers13.com/wp-content/uploads/2015/12/Scenery-Lake-Sunrises-and-sunsets-Sky-Tree-stump-Grass-Nature-410519-730x456.jpg",
+    url: "https://picsum.photos/seed/picsum/200/300",
+    stat: "",
+  },
+  {
+    id: " hidfuvhdfuivhd",
+    name: "baba",
+    url: "",
+    stat: "",
+  },
+  {
+    id: "juihih",
+    name: "baba",
+    url: "",
+    stat: "",
+  },
+  {
+    id: "jinikodfvhfuivn",
+    name: "baba",
+    url: "",
+    stat: "",
+  },
+  {
+    id: "fjiovdjvjiodf",
+    name: "baba",
+    url: "",
+    stat: "",
+  },
+  {
+    id: " xsxzdidzuihd",
+    name: "baba",
+    url: "",
     stat: "",
   },
 ];
 //  Temporary array will be fetched data from props
 
 function ShowPictures() {
-  // Count for limit turn to 2 clicks on different cards
-  const count = useRef(0);
-  // Save the img src for comparing if the cards match
-  const [clickedSrc, setClickedSrc] = useState([]);
-  // Savec the id of each cards to verify it's not the same card clicked
-  const [clickedId, setClickedId] = useState([]);
-  // Save all the img to an array
-  const [card, setCard] = useState([]);
-  // Start game when all the card are ready on useEffect
-  const [ready, setReady] = useState();
-  const [flipped, setFlipped] = useState(false);
+  const newArray = (count) => {
+    return imageList.map((obj, index) => {
+      // eslint-disable-next-line no-dupe-keys
+      return { ...obj, id: index + count };
+    });
+  };
+  // const newArray = imageList.map((obj, index) => {
+  //   // eslint-disable-next-line no-dupe-keys
+  //   return { ...obj, id: index + 1 };
+  // });
+  const [cards] = useState(shuffle([...newArray(1), ...newArray(99)])); // shuffle cards everytime
+  const [clickedImg, setClickedImg] = useState([]); // the chosen img
+  const [matchedCards, setMatchedCards] = useState([]); // array of identical imgs
+  const [pairNum, setpairNum] = useState(0); // this shows how many times the player clicked (2 clicks = 1 turn)
+  const [num, setNum] = useState(1);
 
-  // function to reset the clicked card
-  const resetClicked = () => {
-    setClickedId([]);
-    setClickedSrc([]);
-  };
-  // function to set the clickedsrc and clickedid to the one that are clicked
-  const setClicked = (event) => {
-    setClickedId((oldId) => [...oldId, event.target.id]);
-    setClickedSrc((oldSrc) => [...oldSrc, event.target.name]);
-  };
-  // limit the click to 2
-  const clikedImg = (event) => {
-    if (count.current < 2) {
-      setClicked(event);
-      count.current += 1;
-    }
-  };
-  // call the clickedImg function on click
-  const handleClick = (event) => {
-    clikedImg(event);
-    setFlipped(true);
-  };
+  // when a card is selected, it stays open until we make a second choice.
+  // It no match, both cards flip back
 
-  // Map through given aray and create all the cards
-  const createCard = (array) => {
-    array.map(({ url }) =>
-      setCard((oldCard) =>
-        [
-          ...oldCard,
-          <div className={`card-outer ${flipped === true ? "flipped" : ""}`}>
-            <div className="card">
-              <input
-                className="back"
-                type="image"
-                key={`${uuidv4()}`}
-                id={`${uuidv4()}`}
-                alt=""
-                src=""
-                name={url}
-                onClick={(event) => handleClick(event)}
-              />
-              ,
-              <input
-                className="front "
-                type="image"
-                // onClick={(event) => handleClick(event)}
-                onDragStart={(e) => e.preventDefault()}
-                alt="memorycard"
-                key={`${uuidv4()}`}
-                name={`${url}`}
-                id={`${uuidv4()}`}
-                src={`${url}`}
-              />
-              ,
-            </div>
-            ,
-          </div>,
-        ].sort(() => Math.random() - 0.5)
-      )
-    );
-    // set the state to ready for useeffect
-    setReady(true);
-  };
+  const flipCard = (index) => {
+    if (clickedImg.length === 0) {
+      setClickedImg([index]);
+    } else if (clickedImg.length === 1) {
+      const firstChoice = clickedImg[0];
+      const secondChoice = index;
 
-  //  When clickedSrc is set compare the cards to see if the src match and the id is different
-  useEffect(() => {
-    if (count.current === 2) {
-      count.current = 0;
-      if (clickedSrc[0] === clickedSrc[1]) {
-        if (clickedId[0] !== clickedId[1]) {
-          resetClicked();
-          console.info("TRUE");
-        } else {
-          resetClicked();
-          console.info("FALSE");
-        }
+      if (cards[firstChoice] === cards[secondChoice]) {
+        setMatchedCards([...matchedCards, firstChoice, secondChoice]);
+        console.warn("match !");
       } else {
-        resetClicked();
-        console.info("FALSE");
+        console.warn("not a match !");
       }
+      setClickedImg([...clickedImg, index]);
+    } else if (clickedImg.length === 2) {
+      setClickedImg([index]);
     }
-  }, [clickedSrc]);
+  };
 
-  // create the cards when the data are ready
-  useEffect(() => {
-    createCard(imageList);
-  }, [ready]);
+  // when the player finds all the pairs, an animation of a princess shows up
+  if (matchedCards.length === 18) {
+    return (
+      <div>
+        <input
+          type="image"
+          id="princess"
+          className=""
+          alt="princess img"
+          src=""
+        />
+      </div>
+    );
+  }
+
+  const turns = () => {
+    setNum(num + 1);
+    if (num % 2 === 0) {
+      setpairNum(pairNum + 1);
+    }
+  };
 
   return (
-    <div onDragStart={(e) => e.preventDefault()}>
-      <div onDragStart={(e) => e.preventDefault()} className="imageGrid">
-        {card}
+    <>
+      <div className="imageGrid">
+        {cards.map((card, index) => {
+          const displayedCard =
+            clickedImg.indexOf(index) !== -1 ||
+            matchedCards.indexOf(index) !== -1;
+          return (
+            /* eslint-disable */
+            <div
+              key={card.id}
+              className={`card-outer ${displayedCard ? "flipped" : ""}`}
+              onClick={() => {
+                flipCard(index);
+                turns();
+              }}
+            >
+              <div className="card">
+                <div className="front">
+                  <input
+                    type="image"
+                    className="front"
+                    onDragStart={(e) => e.preventDefault()}
+                    alt="memorycard"
+                    key={`${uuidv4()}`}
+                    src={card.url}
+                    id={`${uuidv4()}`}
+                  />
+                </div>
+                <div className="back">
+                  <input type="image" src="" key={`${uuidv4()}`} alt="" />
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
+      <StopWatch setMatchedCards={setMatchedCards} />
+      <div className="ClickCounterBtn">
+        <button className="myButton" type="submit">
+          {pairNum} Turns
+        </button>
+      </div>
+    </>
   );
 }
 export default ShowPictures;
